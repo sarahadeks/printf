b20d1b1fa09342f6bdb36f4a815dc7aeabcb252e
Opeyemi Adekunle <sarahopeyemi@yahoo.com>
2023-04-04 12:49:17 +0100

    Update and rename parse_char.c to func_char.c

--- func_char.c
+++ func_char.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * parse_char - returns the character
+ * func_char - returns the character
  * @buff_dest: buffer
  * @arg: list of arguments
  * @buff_count: index of buffer pointer
  * Return: buffer index
  */
-int parse_char(char *buff_dest, va_list arg, int buff_count)
+int func_char(char *buff_dest, va_list arg, int buff_count)
 {
 	char c = va_arg(arg, int);
 
