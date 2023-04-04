4b695446eddc92c37ff882681ed0a02aa28a314e
Opeyemi Adekunle <sarahopeyemi@yahoo.com>
2023-04-04 12:54:50 +0100

    Update and rename parse_perc.c to func_perc.c

--- func_perc.c
+++ func_perc.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * parse_perc - returns the character
+ * func_perc - returns the character
  * @buff_dest: character
  * @arg: list of arguments
  * @buff_count: index of buffer pointer
  * Return: the buffer index
  */
-int parse_perc(char *buff_dest, va_list arg, int buff_count)
+int func_perc(char *buff_dest, va_list arg, int buff_count)
 {
 	char c = va_arg(arg, int);
 
